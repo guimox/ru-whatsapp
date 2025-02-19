076be404a8cb8b35ce2f6fe6bb656db1ddc9459b
Guilherme Lopes <guilhermxlopes@gmail.com>
2025-02-19 10:21:13 +0100

    fix: testing with a new structure of icons naming

--- util/constants.js
+++ util/constants.js
@@ -1,12 +1,18 @@
 const iconsMap = {
   'Simbolo-vegano-300x300': '🌱',
   'Origem-animal-site': '🥩',
-  '476bf979-2cbb-476b-8739-02ed26485235': '🐷', // name file is like this in wp ru ufpr
+  '476bf979-2cbb-476b-8739-02ed26485235': '🐷',
   'Gluten-site': '🌾',
   'Leite-e-derivados-site': '🥛',
   'Ovo-site': '🍳',
   'Alergenicos-site': '⚠️',
   'Simbolo-pimenta-300x300': '🌶️',
+  vegan: '🌱',
+  carne: '🥩',
+  gluten: '🌾',
+  lactose: '🥛',
+  ovo: '🍳',
+  'daf04cd5-bacd-4ea4-91ce-48ea45cb0ac4': '🐷',
 };
 
 const daysOfWeek = [
